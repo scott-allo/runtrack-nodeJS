384d04b5c4811dcc67262d27284c7de4ba802a54
scott <scott.allo@laplateforme.io>
2025-06-16 15:57:48 +0200

    Job04 du jour01 terminé! module Fs qui affiche les dossiers présent dans le répertoire

---
+++ jour01/job04/index.js
@@ -0,0 +1,8 @@
+
+const fs = require('fs');
+
+const fichiers = fs.readdirSync('./');
+console.log('Contenu du répertoire courant :');
+fichiers.forEach(fichier => {
+  console.log(`- ${fichier}`);
+});
